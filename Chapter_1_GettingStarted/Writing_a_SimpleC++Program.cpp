cd144c1a26e34380844c545956aa892e717b8db6
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-08-08 07:59:24 +0600

    Writing a Simple C++ Program

---
+++ Chapter_1_GettingStarted/Writing_a_SimpleC++Program.cpp
@@ -0,0 +1,28 @@
+// Every C++ program contains one or more functions, one of which must be named main
+// The operating system runs a C++ program by calling main function
+
+// A function definition has four elements
+// 01. return type -> int
+// 02. function name -> main
+// 03. parameter list enclosed in parentheses -> ()
+// 04. function body -> {}
+
+// C++ start
+int main()
+{
+    return 0;
+    //  return a value to the operating system that terminates a function
+    //  A return 0 means that the program will execute successfully
+    //  A return 1 means that there is some error while executing the program
+}
+
+// Compiling and Executing Our Program
+// The most popular C++ and C compilers are g++ and gcc.
+// -o compiler argument
+// .exe Executing in program
+
+// There are two different kinds of source files:
+// 01. header files
+// 02. non-header files.
+// Header files are generally given names ending in “ .h ”.
+// Non-header files are generally given names ending in “ .cpp, .cc, .cxx, .cpp, .cp,” for C++ code and “ .c ” for C code.
